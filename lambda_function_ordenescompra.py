b0d6d18932e96c116a18b2254be22f26b15b5263
Ricardouchub <rockevans@gmail.com>
2025-07-30 19:27:22 -0400

    Update lambda_function_ordenescompra.py

--- lambda_function_ordenescompra.py
+++ lambda_function_ordenescompra.py
@@ -10,9 +10,9 @@ def lambda_handler(event, context):
     Función Lambda que obtiene las ÓRDENES DE COMPRA de la API 
     y las guarda en un nuevo archivo CSV diario en un bucket de S3.
     """
-    # --- Cargar configuración desde variables de entorno ---
-    NOMBRE_BUCKET = os.environ.get('S3_BUCKET_NAME')
-    MI_TICKET = os.environ.get('API_TICKET')
+    # Cargar configuración desde variables de entorno 
+    NOMBRE_BUCKET = os.environ.get('NOMBRE_S3_BUCKET')
+    MI_TICKET = os.environ.get('CAMBIAR_POR_TU_KEY_AQUI')
     
     # Generar el nombre del archivo
     fecha_hoy_str = datetime.now().strftime('%Y-%m-%d')
@@ -20,7 +20,7 @@ def lambda_handler(event, context):
     
     print(f"Iniciando recolección para el archivo: {nombre_archivo}")
 
-    # --- Consultar la API de Órdenes de Compra ---
+    # Consultar la API de Órdenes de Compra
     url_api = f"https://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.json?ticket={MI_TICKET}"
     response = requests.get(url_api, timeout=60)
     
@@ -35,17 +35,20 @@ def lambda_handler(event, context):
         print("No se encontraron nuevas órdenes de compra. Finalizando.")
         return {'statusCode': 200, 'body': json.dumps('No hay datos nuevos')}
 
-    # --- Guardar en S3 ---
+    # Inicializar el cliente de S3
     s3_client = boto3.client('s3')
+
+    # Convertir el DataFrame a formato CSV en memoria
     csv_buffer = df_nuevos.to_csv(index=False)
     
+    # Subir el archivo CSV al bucket de S3
     s3_client.put_object(
         Bucket=NOMBRE_BUCKET,
         Key=nombre_archivo,
         Body=csv_buffer
     )
     
-    print(f"✅ Proceso completado. {len(df_nuevos)} registros guardados en '{nombre_archivo}'.")
+    print(f"Proceso completado. {len(df_nuevos)} registros guardados en '{nombre_archivo}'.")
     
     return {
         'statusCode': 200,
